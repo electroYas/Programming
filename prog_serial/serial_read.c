625dfd5f2d2c502ded09b5f0893347ae511e1a4b
Yasinda Nimal <ydnmyst@gmail.com>
2015-03-30 00:17:19 +0700

    Update serial_read.c

--- prog_serial/serial_read.c
+++ prog_serial/serial_read.c
@@ -1,12 +1,23 @@
 #include <stdio.h>   /* Standard input/output definitions */
 #include <termios.h> /* POSIX terminal control definitions */
 #include <fcntl.h>   /* File control definitions */
+#include <sys/signal.h>
 
-char buff;
+
+void signal_handler_IO (int status);   /* definition of signal handler */
+
+char ch;
 int n;
+int baud_rate = B115200;
+struct termios options;
+struct sigaction saio;
+ int fd; /* File descriptor for the port */
+
+  
 int main(){
-  int fd; /* File descriptor for the port */
-  int ch;
+  
+  setbuf(stdout, NULL);//this will print charactor on stdout without newline
+ 
   fd = open("/dev/ttyACM0", O_RDWR | O_NOCTTY | O_NDELAY);
   
   if (fd == -1)
@@ -17,18 +28,37 @@ int main(){
     
   }
   
+  saio.sa_handler = signal_handler_IO;
+  saio.sa_flags = 0;
+  saio.sa_restorer = NULL; 
+  sigaction(SIGIO,&saio,NULL);
+  
+  fcntl(fd, F_SETFL, FNDELAY);
+  fcntl(fd, F_SETOWN, getpid());
+  fcntl(fd, F_SETFL,  O_ASYNC );
+  
+  
+  tcgetattr(fd, &options);
+  cfsetispeed(&options, baud_rate);
+  cfsetospeed(&options, baud_rate);
+
+  tcsetattr(fd, TCSANOW, &options);
+
+  
   while(1){
-    
-    while((n=read(fd,&buff,1))>0){
-      printf("%c",buff);
-      printf("\n");
-      
-    }  
-    
-  }
+    sleep(5000);   
+  }  
   
   close(fd);
   
   return 0;
   
+}
+
+void signal_handler_IO (int status)
+{
+   while((n=read(fd,&ch,1))>0){
+      putchar(ch);
+   
+   }
 }

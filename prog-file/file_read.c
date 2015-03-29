e10aaf82e7a7dca79b8a0075a74a28b81a96bc12
Yasinda Nimal <ydnmyst@gmail.com>
2015-03-29 21:36:48 +0700

    Create file_read.c

---
+++ prog-file/file_read.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+   FILE *fp;
+   char buff[255];
+
+   fp = fopen("test.txt", "r");
+   
+   if(fp == NULL){
+      printf("Error!\n");
+      return(-1);
+   }
+   
+   while(fgets(buff, 255, fp)){
+     printf("%s", buff );
+   }
+   
+   printf("\n");
+  
+   fclose(fp);
+
+}
